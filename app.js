32b57b4a70db7c21a15b8f1e37c0e26c507adb79
DariuszLegizynski <d.legizynski@gmail.com>
2020-04-29 22:09:13 +0200

    add ejs and learn how to make a template out of it

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.send("Hello Master");
+    
+    let today = new Date();
+    let currentDay = today.getDay();
+    let day = "";
+
+    if(currentDay === 0){
+        day = "sunday";
+    }else if(currentDay === 1){
+        day = "monday";
+    }else if(currentDay === 2){
+        day = "tuesday";
+    }else if(currentDay === 3){
+        day = "wednesday";
+    }else if(currentDay === 4){
+        day = "thursday";
+    }else if(currentDay === 5){
+        day = "friday";
+    }else if(currentDay === 6){
+        day = "saturday";
+    }else{
+        day = "you made it out of time. Please take care of yourself by yourself. Good Luck!";
+    }
+    
+    res.render("list", {kindOfDay: day}); 
 });
 
 
