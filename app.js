9b59ee6a0b47b37db89bfe1b3016be332cf7914f
DariuszLegizynski <d.legizynski@gmail.com>
2020-04-30 22:34:46 +0200

    add an ejs layout (footer + header) and also add a js module

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 let newlyAddedItems = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get("/", function(req, res){
     
-    let today = new Date();
+    let day = date();
 
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-    
-    res.render("list", {kindOfDay: day, newListItems: newlyAddedItems}); 
+    res.render("list", {listTitle: day, newListItems: newlyAddedItems}); 
 });
 
 app.post("/", function(req, res){
-    newItem = req.body.newItem;
 
-    newlyAddedItems.push(newItem);
+    let item = req.body.newItem;
 
-    res.redirect("/");
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        newlyAddedItems.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
 })
 
 app.listen(3000, function(){
