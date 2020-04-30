93e223fb907ca49efbd73c46a5ab20b75fb4c8ce
DariuszLegizynski <d.legizynski@gmail.com>
2020-04-30 22:46:51 +0200

    refract the .js files

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let newlyAddedItems = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const newlyAddedItems = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.get("/", function(req, res){
     
-    let day = date();
+    const day = date.getDate();
 
     res.render("list", {listTitle: day, newListItems: newlyAddedItems}); 
 });
 
 app.post("/", function(req, res){
 
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if(req.body.list === "Work"){
         workItems.push(item);
